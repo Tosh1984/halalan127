9fc7d70da9b3445d78da9300d0d40565a45d1cbc
millany <38325297+millany@users.noreply.github.com>
2019-05-01 15:43:20 +0800

    Add files via upload

---
+++ user_login.php
@@ -0,0 +1,20 @@
+<html>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>User Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <h1>Voter Login</h1>
+    <form action="admin_login.php" method="post">
+        <!-- 自分情報を入力 -->
+        <label>username: </label>
+        <input type="text" name="username"><br>
+        <label>password: </label>
+        <input type="password" name="password"><br>
+        <input type="submit" name="Submit" value="Submit"/><br>
+    </form>
+</body>
+</html>
