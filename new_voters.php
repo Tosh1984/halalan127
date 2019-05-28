e366a0bbccf392370a7fb536ba583f02e797c408
Alex Yeo <amyeo@up.edu.ph>
2019-05-28 13:48:47 +0800

    fix js links

--- new_voters.php
+++ new_voters.php
@@ -34,7 +34,7 @@ if(mysqli_connect_errno()){
     <link rel="stylesheet" href="/boostrap/css/bootstrap.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
     <title> Add new voters </title>
