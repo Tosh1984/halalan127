2fdbaa69c6466a830a4fd5bbef8ce5fbe3d149cf
Alex Yeo <amyeo@up.edu.ph>
2019-05-28 13:46:42 +0800

    add static bootstrap

--- new_subelection.php
+++ new_subelection.php
@@ -53,7 +53,7 @@ $result = mysqli_fetch_assoc(mysqli_query($link, $sel_query));
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/boostrap/css/bootstrap.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
